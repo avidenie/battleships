cb08b5e85da9db1a3fc520ec3f9ea0254cdcad35
Adrian Videnie <avidenie@gmail.com>
2023-06-19 23:16:37 +0300

    feat: use FittedBox for board clues

--- lib/widgets/remaining.dart
+++ lib/widgets/remaining.dart
@@ -16,20 +16,26 @@ class Remaining extends StatelessWidget {
         child: Builder(
           builder: (context) {
             if (remaining == 0) {
-              return const Icon(
-                Icons.check_rounded,
-                size: 16.0,
-                color: Colors.green,
+              return const FittedBox(
+                child: Icon(
+                  Icons.check_rounded,
+                  size: 16.0,
+                  color: Colors.green,
+                ),
               );
             }
             if (remaining < 0) {
-              return const Icon(
-                Icons.close,
-                size: 16.0,
-                color: Colors.red,
+              return const FittedBox(
+                child: Icon(
+                  Icons.close,
+                  size: 16.0,
+                  color: Colors.red,
+                ),
               );
             }
-            return Text(remaining.toString(),
+            return FittedBox(
+              child: Text(
+                remaining.toString(),
                 style: Theme.of(context).textTheme.titleSmall?.copyWith(
                     color: remaining >= 0
                         ? Theme.of(context)
@@ -37,7 +43,9 @@ class Remaining extends StatelessWidget {
                             .titleSmall
                             ?.color
                             ?.withAlpha(200)
-                        : Colors.red));
+                        : Colors.red),
+              ),
+            );
           },
         ),
       ),
